bb155f40910dd3252d9033b756fd3053f636996a
Ali <Zhanabekov.ali@gmail.com>
2023-09-04 23:16:47 +0600

    Updates

---
+++ static/js/7.ca8edfbe.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmarvel=this.webpackJsonpmarvel||[]).push([[7],{68:function(t,e,n){"use strict";n.r(e);var c=n(10),i=n(24),s=n(1);e.default=function(){return Object(s.jsxs)("div",{children:[Object(s.jsx)(i.a,{}),Object(s.jsx)(c.b,{style:{display:"block",textAlign:"center",fontWeight:"bold",fontSize:"24px"},to:"/",children:"Back to Main page"})]})}}}]);
+//# sourceMappingURL=7.ca8edfbe.chunk.js.map
